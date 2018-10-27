5a3597ff7697be5f0a287a8162540d805340a5b5
Abhinav Sharma <abhinav0301@gmail.com>
2018-10-27 13:26:36 -0400

    added data, code and results for fanova-mnist

---
+++ data/prep-data.py
@@ -0,0 +1,21 @@
+import csv
+import math
+
+with open('mnist-responses.csv', 'w') as csvfile:
+	writer = csv.writer(csvfile, delimiter=',')#,quotechar='"', quoting=csv.QUOTE_MINIMAL)
+	f = open('output-all.txt', 'r')
+	lines = f.readlines()
+	for line in lines:
+		words = line.split()
+		res = [words[1]]
+		# res = []
+		# res.append(int(math.log(int(words[5][:-1]),2)))
+		# res.append(words[7][:-1])
+		# res.append(words[9][:-1])
+		# res.append(words[11][:-1])
+		# if words[13][:-1]=='False':
+		# 	res.append(1)
+		# else:
+		# 	res.append(0)
+		# res.append(words[15][:-1])
+		writer.writerow(res)
