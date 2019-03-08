8a1d0692c512fe3b347a774c8dab120ad6fbd645
Sharma <abhinav0301@gmail.com>
2019-03-07 16:39:40 -0800

    updated comp_importance

--- comp_importance.py
+++ comp_importance.py
@@ -601,7 +601,6 @@ def get_hyperparameter_search_space_flower(seed=None):
 
 dataset = ['cifar10', 'cifar100', 'fmnist', 'mnist', 'scmnist', 'dvc', 'svhn', 'fruits', 'stl10', 'flower']
 response_type = ['acc', 'time']
-dic = {'0':'batch_size','1':'epochs','2':'h_flip','3':'learning_rate_init','4':'lr_decay','5':'momentum','6':'patience','7':'resize_crop','8':'shuffle','9':'tolerance','10':'v_flip','11':'weight_decay'}
 os.mkdir('imps')
 for d in dataset:
     print(d)
@@ -623,24 +622,3 @@ for d in dataset:
             dims = (i, )
             res = f.quantify_importance(dims)
             fil.write(str(res)+'\n')
-
-with open('importance-single-12params.csv', 'w') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',')#,quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(['dataset', 'parameter', 'marginal_contribution_accuracy', 'marginal_contribution_runtime'])
-    d = os.listdir('imps/')
-    for i in d:
-        #print(i)
-        f = open('imps/'+i, 'r')
-        lines = f.readlines()
-        for line in range(12):
-            words = lines[line].split()
-            #print(words[0][2:4])
-            words_t = lines[line+12].split()
-            res = [i.split('-')[1][:-4]]
-            if words[0][3] == ',':
-                res.append(dic[words[0][2]])
-            else:
-                res.append(dic[words[0][2:4]])
-            res.append(words[3][:-1])
-            res.append(words_t[3][:-1])
-            writer.writerow(res)
