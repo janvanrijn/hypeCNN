2dd81b13d657aae7e9dc4b9b4c35969f2e127d39
janvanrijn <janvanrijn@gmail.com>
2019-03-27 13:24:46 -0400

    merges configspaces

--- config_spaces/__init__.py
+++ config_spaces/__init__.py
@@ -1,13 +1,9 @@
-from . import cifar10
-from . import cifar100
+from . import cifar_scmnist_svhn  # covers cifar10, cifar100, svhn
 from . import dvc
 from . import flower
-from . import fmnist
 from . import fruits
-from . import mnist
-from . import scmnist
+from . import mnist  # covers mnist and fmnist
 from . import stl10
-from . import svhn
 
 
 DATASETS = [
@@ -26,24 +22,24 @@ DATASETS = [
 
 def get_config_space(dataset, seed):
     if dataset == 'cifar10':
-        return cifar10.get_hyperparameter_search_space(seed)
+        return cifar_scmnist_svhn.get_hyperparameter_search_space(seed)
     elif dataset == 'cifar100':
-        return cifar100.get_hyperparameter_search_space(seed)
+        return cifar_scmnist_svhn.get_hyperparameter_search_space(seed)
     elif dataset == 'dvc':
         return dvc.get_hyperparameter_search_space(seed)
     elif dataset == 'flower':
         return flower.get_hyperparameter_search_space(seed)
     elif dataset == 'fmnist':
-        return fmnist.get_hyperparameter_search_space(seed)
+        return mnist.get_hyperparameter_search_space(seed)
     elif dataset == 'fruits':
         return fruits.get_hyperparameter_search_space(seed)
     elif dataset == 'mnist':
         return mnist.get_hyperparameter_search_space(seed)
     elif dataset == 'scmnist':
-        return scmnist.get_hyperparameter_search_space(seed)
+        return cifar_scmnist_svhn.get_hyperparameter_search_space(seed)
     elif dataset == 'stl10':
         return stl10.get_hyperparameter_search_space(seed)
     elif dataset == 'svhn':
-        return svhn.get_hyperparameter_search_space(seed)
+        return cifar_scmnist_svhn.get_hyperparameter_search_space(seed)
     else:
         raise ValueError('Unknown dataset: %s' % dataset)
